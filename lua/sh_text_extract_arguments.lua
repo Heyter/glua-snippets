c89812c2ac1ff51383f7a59aea253532e515f879
Sam Nyashev <12237569+Heyter@users.noreply.github.com>
2024-12-05 08:13:48 +0300

    Update sh_text_extract_arguments.lua

--- lua/sh_text_extract_arguments.lua
+++ lua/sh_text_extract_arguments.lua
@@ -1,46 +1,39 @@
-local strsub, strbyte, strmatch = string.sub, string.byte, string.match
+local strsub, strbyte = string.sub, string.byte
 local arguments = setmetatable({}, { __mode = "v" })
 
--- UTF-8 support
+-- GoodOldBrick
+-- " - 0x22 -- doublequotes
+--   - 0x20 -- whitespace
 function string.ExtractArgs(text, max)
-	local arglen, curString = 0, ""
-	local pos, charbytes = 1
+	local ptr, len, arglen = 1, #text, 0
 
-	while pos <= #text do
-		local b = strbyte(text, pos)
-		if b < 0x80 then charbytes = 1
-		elseif b < 0xE0 then charbytes = 2
-		elseif b < 0xF0 then charbytes = 3
-		elseif b < 0xF8 then charbytes = 4
-		else charbytes = 1 end
-		local c = strsub(text, pos, pos + charbytes - 1)
+	while ptr <= len do
+		while ptr <= len and strbyte(text, ptr) == 0x20 do
+			ptr = ptr + 1
+		end
 
-		if c == '"' then
-			local match = strmatch(strsub(text, pos), '%b""')
-
-			if match then
-				curString = ""
-				pos = pos + #match
-				arglen = arglen + 1
-				arguments[arglen] = strsub(match, 2, -2)
-			else curString = curString .. c end
-		elseif c == " " and curString ~= "" then
+		if ptr > len then break end
+		if strbyte(text, ptr) == 0x22 then
+			local sub = ptr + 1
+			ptr = sub
+			while ptr <= len and strbyte(text, ptr) ~= 0x22 do
+				ptr = ptr + 1
+			end
 			arglen = arglen + 1
-			arguments[arglen] = curString
-			curString = ""
-		elseif curString == "" and c == " " then
-		else curString = curString..c end
+			arguments[arglen] = strsub(text, sub, ptr - 1)
+			ptr = ptr + 1
+		else
+			local sub = ptr
+			while ptr <= len and strbyte(text, ptr) ~= 0x20 and strbyte(text, ptr) ~= 0x22 do
+				ptr = ptr + 1
+			end
+			arglen = arglen + 1
+			arguments[arglen] = strsub(text, sub, ptr - 1)
+		end
 
 		if max and arglen >= max then
 			return arguments, arglen
 		end
-
-		pos = pos + charbytes
-	end
-
-	if curString ~= "" then
-		arglen = arglen + 1
-		arguments[arglen] = curString
 	end
 
 	return arguments, arglen
@@ -48,3 +41,4 @@ end
 
 -- local a, b = string.ExtractArgs('/setname "John Smith"', 1)
 -- local a, b = string.ExtractArgs('/test "Привет мир" 100')
+-- local a, b = string.ExtractArgs('/test"Привет мир"100') -- also work
